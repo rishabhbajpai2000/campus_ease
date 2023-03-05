d318d068fb569d076354e09ab7dd96fc21d48af9
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-03-06 01:08:45 +0530

    added the logic to calculate the days you can take Off in order to not get detained from exams.

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+DateTime kSemStartDate =
+    DateTime(2023, 2, 6); // need to be taken when user logs in for first time
+DateTime kExamStartDate = DateTime(2023, 2, 28); // same as above.
+int kActualClassesHappened =
+    0; // this will update as the student will enter the data.
+int kTotalClassesNotTaken = 0;
+double kRequiredAttendance = 75.0;
